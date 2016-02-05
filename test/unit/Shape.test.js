e8221c22762d791db03288d4385a982a84b86087
ghaiklor <ghaiklor@gmail.com>
2016-02-05 16:01:52 +0200

    test(shape): Refactor tests

--- test/unit/Shape.test.js
+++ test/unit/Shape.test.js
@@ -4,18 +4,21 @@ import Shape from '../../src/Shape';
 
 describe('Shape', () => {
   it('Should properly initialize shape instance with no arguments', () => {
-    let shape = new Shape();
+    const shape = new Shape();
+
     assert.instanceOf(shape, Shape);
   });
 
   it('Should properly initialize shape instance with arguments', () => {
-    let shape = new Shape({text: 'test'});
+    const shape = new Shape({text: 'test'});
+
     assert.instanceOf(shape, Shape);
     assert.equal(shape.getText(), 'test');
   });
 
   it('Should properly get/set property from the shape', () => {
-    let shape = new Shape();
+    const shape = new Shape();
+
     assert.equal(shape.get('text'), '');
     assert.instanceOf(shape.set('animation.name', 'print'), Shape);
     assert.equal(shape.get('animation.name'), 'print');
@@ -24,14 +27,16 @@ describe('Shape', () => {
   });
 
   it('Should properly get/set text', () => {
-    let shape = new Shape();
+    const shape = new Shape();
+
     assert.equal(shape.getText(), '');
     assert.instanceOf(shape.setText('test'), Shape);
     assert.equal(shape.getText(), 'test');
   });
 
   it('Should properly get/set width', () => {
-    let shape = new Shape();
+    const shape = new Shape();
+
     assert.equal(shape.getWidth(), 15);
     assert.instanceOf(shape.setWidth(5), Shape);
     assert.equal(shape.getWidth(), 5);
@@ -40,7 +45,8 @@ describe('Shape', () => {
   });
 
   it('Should properly get/set height', () => {
-    let shape = new Shape();
+    const shape = new Shape();
+
     assert.equal(shape.getHeight(), 5);
     assert.instanceOf(shape.setHeight(15), Shape);
     assert.equal(shape.getHeight(), 15);
@@ -49,7 +55,8 @@ describe('Shape', () => {
   });
 
   it('Should properly get/set x coordinate', () => {
-    let shape = new Shape();
+    const shape = new Shape();
+
     assert.equal(shape.getX(), 10);
     assert.instanceOf(shape.setX(20), Shape);
     assert.equal(shape.getX(), 20);
@@ -64,7 +71,8 @@ describe('Shape', () => {
   });
 
   it('Should properly get/set y coordinate', () => {
-    let shape = new Shape();
+    const shape = new Shape();
+
     assert.equal(shape.getY(), 10);
     assert.instanceOf(shape.setY(20), Shape);
     assert.equal(shape.getY(), 20);
@@ -79,27 +87,30 @@ describe('Shape', () => {
   });
 
   it('Should properly get/set background', () => {
-    let shape = new Shape();
+    const shape = new Shape();
+
     assert.notOk(shape.getBackground());
     assert.instanceOf(shape.setBackground('red'), Shape);
     assert.equal(shape.getBackground(), 'red');
   });
 
   it('Should properly get/set foreground', () => {
-    let shape = new Shape();
+    const shape = new Shape();
+
     assert.notOk(shape.getBackground());
     assert.instanceOf(shape.setForeground('yellow'), Shape);
     assert.equal(shape.getForeground(), 'yellow');
   });
 
   it('Should properly throw exception if render is not overridden', () => {
-    let shape = new Shape();
+    const shape = new Shape();
+
     assert.throws(() => shape.render(), Error, 'render() method must be implemented');
   });
 
   it('Should properly serialize shape to object', () => {
-    let shape = new Shape();
-    let obj = shape.toObject();
+    const shape = new Shape();
+    const obj = shape.toObject();
 
     assert.deepEqual(obj, {
       type: 'Shape',
@@ -116,8 +127,8 @@ describe('Shape', () => {
   });
 
   it('Should properly serialize shape to object with custom options', () => {
-    let shape = new Shape({text: 'test', x: 0, y: 0, width: 30, height: 50, background: 'red', foreground: 'black'});
-    let obj = shape.toObject();
+    const shape = new Shape({text: 'test', x: 0, y: 0, width: 30, height: 50, background: 'red', foreground: 'black'});
+    const obj = shape.toObject();
 
     assert.deepEqual(obj, {
       type: 'Shape',
@@ -134,37 +145,40 @@ describe('Shape', () => {
   });
 
   it('Should properly serialize shape to JSON', () => {
-    let shape = new Shape();
-    let json = shape.toJSON();
+    const shape = new Shape();
+    const json = shape.toJSON();
 
     assert.equal(json, '{"type":"Shape","options":{"text":"","width":15,"height":5,"x":10,"y":10}}');
   });
 
   it('Should properly serialize shape to JSON with custom options', () => {
-    let shape = new Shape({text: 'test', x: 0, y: 0, width: 30, height: 50});
-    let json = shape.toJSON();
+    const shape = new Shape({text: 'test', x: 0, y: 0, width: 30, height: 50});
+    const json = shape.toJSON();
 
     assert.equal(json, '{"type":"Shape","options":{"text":"test","width":30,"height":50,"x":0,"y":0}}');
   });
 
   it('Should properly create Shape instance from static create()', () => {
-    let shape = Shape.create({text: 'test'});
+    const shape = Shape.create({text: 'test'});
+
     assert.equal(shape.getText(), 'test');
     assert.instanceOf(shape, Shape);
   });
 
   it('Should properly throw error if trying to create Shape not from Object representation', () => {
-    let obj = {};
+    const obj = {};
+
     assert.throws(() => Shape.fromObject(obj), Error, 'It looks like it is not an Object representation of the shape');
   });
 
   it('Should properly throw error if trying to create Shape not from its representation', () => {
-    let obj = {type: 'Rectangle', options: {}};
+    const obj = {type: 'Rectangle', options: {}};
+
     assert.throws(() => Shape.fromObject(obj), Error, 'Rectangle is not an Object representation of the Shape');
   });
 
   it('Should properly create Shape instance from Object representation', () => {
-    let obj = {
+    const obj = {
       type: 'Shape',
       options: {
         text: 'test',
@@ -177,7 +191,8 @@ describe('Shape', () => {
       }
     };
 
-    let shape = Shape.fromObject(obj);
+    const shape = Shape.fromObject(obj);
+
     assert.instanceOf(shape, Shape);
     assert.equal(shape.getText(), 'test');
     assert.equal(shape.getWidth(), 30);
@@ -189,8 +204,8 @@ describe('Shape', () => {
   });
 
   it('Should properly create Shape instance from JSON representation', () => {
-    let json = '{"type":"Shape","options":{"text":"test","width":30,"height":50,"x":1,"y":1}}';
-    let shape = Shape.fromJSON(json);
+    const json = '{"type":"Shape","options":{"text":"test","width":30,"height":50,"x":1,"y":1}}';
+    const shape = Shape.fromJSON(json);
 
     assert.instanceOf(shape, Shape);
     assert.equal(shape.getText(), 'test');
