0f25939565abc8443bf5e45f5a27ce74d067f7fd
ghaiklor <ghaiklor@gmail.com>
2016-03-27 12:29:50 +0300

    test(shape): Fix tests

--- test/unit/Shape.test.js
+++ test/unit/Shape.test.js
@@ -1,23 +1,25 @@
-import { assert } from 'chai';
-import sinon from 'sinon';
+import {assert} from 'chai';
+import Cursor from 'kittik-cursor';
 import Shape from '../../src/Shape';
+
+const cursor = Cursor.create();
 
 describe('Shape', () => {
   it('Should properly initialize shape instance with no arguments', () => {
-    const shape = new Shape();
+    const shape = new Shape(cursor);
 
     assert.instanceOf(shape, Shape);
   });
 
   it('Should properly initialize shape instance with arguments', () => {
-    const shape = new Shape({text: 'test'});
+    const shape = new Shape(cursor, {text: 'test'});
 
     assert.instanceOf(shape, Shape);
     assert.equal(shape.getText(), 'test');
   });
 
   it('Should properly get/set property from the shape', () => {
-    const shape = new Shape();
+    const shape = new Shape(cursor);
 
     assert.equal(shape.get('text'), '');
     assert.instanceOf(shape.set('animation.name', 'print'), Shape);
@@ -27,7 +29,7 @@ describe('Shape', () => {
   });
 
   it('Should properly get/set text', () => {
-    const shape = new Shape();
+    const shape = new Shape(cursor);
 
     assert.equal(shape.getText(), '');
     assert.instanceOf(shape.setText('test'), Shape);
@@ -35,27 +37,27 @@ describe('Shape', () => {
   });
 
   it('Should properly get/set width', () => {
-    const shape = new Shape();
+    const shape = new Shape(cursor);
 
     assert.equal(shape.getWidth(), 15);
     assert.instanceOf(shape.setWidth(5), Shape);
     assert.equal(shape.getWidth(), 5);
     assert.instanceOf(shape.setWidth('50%'), Shape);
-    assert.equal(shape.getWidth(), Math.ceil(process.stdout.columns / 2));
+    assert.equal(shape.getWidth(), Math.ceil(cursor._width / 2));
   });
 
   it('Should properly get/set height', () => {
-    const shape = new Shape();
+    const shape = new Shape(cursor);
 
     assert.equal(shape.getHeight(), 5);
     assert.instanceOf(shape.setHeight(15), Shape);
     assert.equal(shape.getHeight(), 15);
     assert.instanceOf(shape.setHeight('50%'), Shape);
-    assert.equal(shape.getHeight(), Math.floor(process.stdout.rows / 2));
+    assert.equal(shape.getHeight(), Math.floor(cursor._height / 2));
   });
 
   it('Should properly get/set x coordinate', () => {
-    const shape = new Shape();
+    const shape = new Shape(cursor);
 
     assert.equal(shape.getX(), 10);
     assert.instanceOf(shape.setX(20), Shape);
@@ -63,15 +65,15 @@ describe('Shape', () => {
     assert.instanceOf(shape.setX('left'), Shape);
     assert.equal(shape.getX(), 0);
     assert.instanceOf(shape.setX('center'), Shape);
-    assert.equal(shape.getX(), Math.floor(process.stdout.columns / 2 - shape.getWidth() / 2));
+    assert.equal(shape.getX(), Math.floor(cursor._width / 2 - shape.getWidth() / 2));
     assert.instanceOf(shape.setX('right'), Shape);
-    assert.equal(shape.getX(), Math.floor(process.stdout.columns - shape.getWidth()));
+    assert.equal(shape.getX(), Math.floor(cursor._width - shape.getWidth()));
     assert.instanceOf(shape.setX('50%'), Shape);
-    assert.equal(shape.getX(), Math.floor(process.stdout.columns / 2));
+    assert.equal(shape.getX(), Math.floor(cursor._width / 2));
   });
 
   it('Should properly get/set y coordinate', () => {
-    const shape = new Shape();
+    const shape = new Shape(cursor);
 
     assert.equal(shape.getY(), 10);
     assert.instanceOf(shape.setY(20), Shape);
@@ -79,15 +81,15 @@ describe('Shape', () => {
     assert.instanceOf(shape.setY('top'), Shape);
     assert.equal(shape.getY(), 0);
     assert.instanceOf(shape.setY('middle'), Shape);
-    assert.equal(shape.getY(), Math.floor(process.stdout.rows / 2 - shape.getHeight() / 2));
+    assert.equal(shape.getY(), Math.floor(cursor._height / 2 - shape.getHeight() / 2));
     assert.instanceOf(shape.setY('bottom'), Shape);
-    assert.equal(shape.getY(), Math.floor(process.stdout.rows - shape.getHeight()));
+    assert.equal(shape.getY(), Math.floor(cursor._height - shape.getHeight()));
     assert.instanceOf(shape.setY('50%'), Shape);
-    assert.equal(shape.getY(), Math.floor(process.stdout.rows / 2));
+    assert.equal(shape.getY(), Math.floor(cursor._height / 2));
   });
 
   it('Should properly get/set background', () => {
-    const shape = new Shape();
+    const shape = new Shape(cursor);
 
     assert.notOk(shape.getBackground());
     assert.instanceOf(shape.setBackground('red'), Shape);
@@ -95,7 +97,7 @@ describe('Shape', () => {
   });
 
   it('Should properly get/set foreground', () => {
-    const shape = new Shape();
+    const shape = new Shape(cursor);
 
     assert.notOk(shape.getBackground());
     assert.instanceOf(shape.setForeground('yellow'), Shape);
@@ -103,13 +105,13 @@ describe('Shape', () => {
   });
 
   it('Should properly throw exception if render is not overridden', () => {
-    const shape = new Shape();
+    const shape = new Shape(cursor);
 
     assert.throws(() => shape.render(), Error, 'render() method must be implemented');
   });
 
   it('Should properly serialize shape to object', () => {
-    const shape = new Shape();
+    const shape = new Shape(cursor);
     const obj = shape.toObject();
 
     assert.deepEqual(obj, {
@@ -127,7 +129,15 @@ describe('Shape', () => {
   });
 
   it('Should properly serialize shape to object with custom options', () => {
-    const shape = new Shape({text: 'test', x: 0, y: 0, width: 30, height: 50, background: 'red', foreground: 'black'});
+    const shape = new Shape(cursor, {
+      text: 'test',
+      x: 0,
+      y: 0,
+      width: 30,
+      height: 50,
+      background: 'red',
+      foreground: 'black'
+    });
     const obj = shape.toObject();
 
     assert.deepEqual(obj, {
@@ -145,21 +155,21 @@ describe('Shape', () => {
   });
 
   it('Should properly serialize shape to JSON', () => {
-    const shape = new Shape();
+    const shape = new Shape(cursor);
     const json = shape.toJSON();
 
     assert.equal(json, '{"type":"Shape","options":{"text":"","width":15,"height":5,"x":10,"y":10,"background":false,"foreground":false}}');
   });
 
   it('Should properly serialize shape to JSON with custom options', () => {
-    const shape = new Shape({text: 'test', x: 0, y: 0, width: 30, height: 50});
+    const shape = new Shape(cursor, {text: 'test', x: 0, y: 0, width: 30, height: 50});
     const json = shape.toJSON();
 
     assert.equal(json, '{"type":"Shape","options":{"text":"test","width":30,"height":50,"x":0,"y":0,"background":false,"foreground":false}}');
   });
 
   it('Should properly create Shape instance from static create()', () => {
-    const shape = Shape.create({text: 'test'});
+    const shape = Shape.create(cursor, {text: 'test'});
 
     assert.equal(shape.getText(), 'test');
     assert.instanceOf(shape, Shape);
@@ -191,9 +201,10 @@ describe('Shape', () => {
       }
     };
 
-    const shape = Shape.fromObject(obj);
+    const shape = Shape.fromObject(obj, cursor);
 
     assert.instanceOf(shape, Shape);
+    assert.instanceOf(shape.getCursor(), Cursor);
     assert.equal(shape.getText(), 'test');
     assert.equal(shape.getWidth(), 30);
     assert.equal(shape.getHeight(), 50);
@@ -205,9 +216,10 @@ describe('Shape', () => {
 
   it('Should properly create Shape instance from JSON representation', () => {
     const json = '{"type":"Shape","options":{"text":"test","width":30,"height":50,"x":1,"y":1}}';
-    const shape = Shape.fromJSON(json);
+    const shape = Shape.fromJSON(json, cursor);
 
     assert.instanceOf(shape, Shape);
+    assert.instanceOf(shape.getCursor(), Cursor);
     assert.equal(shape.getText(), 'test');
     assert.equal(shape.getWidth(), 30);
     assert.equal(shape.getHeight(), 50);
